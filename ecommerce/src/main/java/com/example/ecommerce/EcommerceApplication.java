43a60fa0cd07f1f27a4a49dc03d3d1186bce68a5
freeredfingerlan5 <freeredfingerlan5@gmail.com>
2024-07-30 04:07:20 +0000

    update

--- ecommerce/src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -1,7 +1,11 @@
 package com.example.ecommerce;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -9,5 +13,10 @@ public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
+
+	@PostConstruct
+	public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 
 }

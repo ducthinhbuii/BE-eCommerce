532a1c531d8e9f26ebbd6f2e3ea4a70306ba1034
freeredfingerlan5 <freeredfingerlan5@gmail.com>
2024-08-10 03:41:11 +0000

    update

--- ecommerce/src/main/java/com/example/ecommerce/firebase/FirebaseConfig.java
+++ ecommerce/src/main/java/com/example/ecommerce/firebase/FirebaseConfig.java
@@ -1,27 +1,27 @@
-package com.example.ecommerce.firebase;
+// package com.example.ecommerce.firebase;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+// import java.io.FileInputStream;
+// import java.io.IOException;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.firebase.FirebaseApp;
+// import com.google.firebase.FirebaseOptions;
 
-@Configuration
-public class FirebaseConfig {
+// @Configuration
+// public class FirebaseConfig {
 
-    @Bean
-    public FirebaseApp initializeFirebase() throws IOException {
-        String serviceAccountPath = System.getProperty("user.dir") + "/eCommerce/back-end/ecommerce/colorShop-firebase-key.json";
-        FileInputStream serviceAccountStream = new FileInputStream(serviceAccountPath);
+//     @Bean
+//     public FirebaseApp initializeFirebase() throws IOException {
+//         String serviceAccountPath = System.getProperty("user.dir") + "/eCommerce/back-end/ecommerce/colorShop-firebase-key.json";
+//         FileInputStream serviceAccountStream = new FileInputStream(serviceAccountPath);
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
-                .setStorageBucket("colorshop-44e58.appspot.com")
-                .build();
-        return FirebaseApp.initializeApp(options);
-    }
-}
+//         FirebaseOptions options = FirebaseOptions.builder()
+//                 .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+//                 .setStorageBucket("colorshop-44e58.appspot.com")
+//                 .build();
+//         return FirebaseApp.initializeApp(options);
+//     }
+// }
